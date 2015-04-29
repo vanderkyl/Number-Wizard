c89440bba8b34beeb70b00470289f44320a1ad1c
ulockitonline <adrian@admins-mac-pro.local>
2015-04-29 17:59:53 -0500

    Initial Commit

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name) {
+		Debug.Log ("Level load requested for: " + name);
+		Application.LoadLevel(name);
+	}
+
+	public void quitRequest() {
+		Debug.Log ("Request to quit made.");
+	}
+}
