5b7377ab8225d3f5e3f5e028968edd91bd79983d
ulockitonline <adrian@admins-mac-pro.local>
2015-05-01 17:50:38 -0500

    Finished rough game

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+	// Use this for initialization
+	public Text text;
+	public Text tries;
+	int max;
+	int min;
+	int guess;
+
+	int maxGuesses = 10;
+
+	void Start () {
+		StartGame();
+	}
+	
+	void StartGame () {
+		max = 1000;
+		min = 1;
+		guess = 500;
+		text.text = "" + guess;
+		tries.text = "" + maxGuesses;
+
+		max = max + 1;
+	}
+
+	public void GuessHigher() {
+		min = guess;
+		NextGuess();
+	}
+
+	public void GuessLower() {
+		max = guess;
+		NextGuess();
+	}
+
+	void NextGuess () {
+		if (--maxGuesses == 0) {
+			Application.LoadLevel("Win");
+		}
+		guess = (max + min) / 2;
+		text.text = "" + guess;
+		tries.text = "" + maxGuesses;
+	}
+}
